4e4aff86ad15a396d15a8c809da96365c46d0984
Igor Kaseko <igorksk@gmail.com>
2025-04-12 12:00:31 +0300

    wip 1

--- backend/CustomersAPI/CustomersAPI/Program.cs
+++ backend/CustomersAPI/CustomersAPI/Program.cs
@@ -9,7 +9,8 @@ namespace CustomersAPI
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-            builder.Services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("CustomersDb"));
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
             builder.Services.AddCors(options =>
@@ -18,6 +19,13 @@ namespace CustomersAPI
             });
 
             var app = builder.Build();
+
+            // Apply migrations
+            using (var scope = app.Services.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                db.Database.Migrate();
+            }
 
             if (app.Environment.IsDevelopment())
             {
